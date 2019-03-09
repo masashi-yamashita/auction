5b6fa1d949fc598ef04cee46cf53f58619a26ec2
masashi-yamashita <rabbit.wonderfuleveryday@gmail.com>
2019-03-09 17:34:09 +0900

    呼び出し元作成

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -6,4 +6,28 @@ class ItemsController < ApplicationController
   def index
     @items = Item.all
   end
+  
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+
+    # 上の行はインスタンスを作っただけ。saveメソッドで保存しないと、消える。
+      @item.save
+
+    # saveの後にshow(商品詳細ページ)に飛ぶ設定をします。
+    # これを設定しないと、プログラムが行き場をなくしてエラーが起きます。
+    redirect_to @item
+
+    # redirect_to "/items"
+    # だとindexページにリダイレクトする
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :price, :seller, :description, :email, :image_url)
+  end
 end
